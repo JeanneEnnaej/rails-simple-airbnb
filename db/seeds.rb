23aff666ecf38adbf828810aebd34bbb12b7c5e6
jeanne desmier <desmierjeanne@gmail.com>
2022-08-17 17:03:50 +0200

    simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Canus in Lyon',
+  address: '15 rue de la Croix Rousse France',
+  description: 'Joli petit appartement sur la coline lyonnaise',
+  price_per_night: 90,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Bouiboui in Paris',
+  address: '2 avenue du Trocadero',
+  description: '3m2 de rêve avec vu sur la tour eiffel',
+  price_per_night: 200,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Loft in La Creuse',
+  address: '4ter impasse de la boue',
+  description: '200m2 à coté des vaches',
+  price_per_night: 30,
+  number_of_guests: 10
+)
